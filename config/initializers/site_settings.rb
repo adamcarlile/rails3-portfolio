f0ad605710f83809aaf50c147494793317816e92
Adam Carlile <adam@benchmedia.co.uk>
2011-01-10 13:46:02 -0800

    Added google maps settings

--- config/initializers/site_settings.rb
+++ config/initializers/site_settings.rb
@@ -1,2 +1,2 @@
 SITE_NAME = "Adam Carlile, Web Professional"
-GOOGLE_MAPS_KEY = YAML.load_file(RAILS_ROOT + '/config/google_maps_keys.yml')[ENV['RAILS_ENV']]
+GOOGLE_MAPS_KEY = YAML.load_file(RAILS_ROOT + '/config/google_maps_keys.yml')[RAILS_ENV]
